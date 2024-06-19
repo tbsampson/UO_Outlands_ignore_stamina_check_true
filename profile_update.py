953f9a2e74c64cb1ebcaa3f75d7dddd060ef1911
Tom Sampson <tbsampson@gmail.com>
2024-06-19 09:48:56 -0500

    added comment

--- profile_update.py
+++ profile_update.py
@@ -1,3 +1,17 @@
+"""
+Script for UO Outlands
+Author: [NKB] Spongebox / Starcaine
+Date: 2024-06-12
+
+Description:
+This script searches within a configurable path (default: C:\\Program Files (x86)\\Ultima Online Outlands)
+and its subdirectories for files named profile.json. It then updates the "ignore_stamina_check" setting
+from false to true in these files. The script requires administrative privileges to run.
+
+Usage:
+Simply run the script. If not running with administrative privileges, it will request them automatically.
+
+"""
 import os
 import sys
 import ctypes
